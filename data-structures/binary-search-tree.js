066a5cca23b5066b446e82d8a8831f5c5b4e303b
Kyle <k.yasumiishi@gmail.com>
2018-08-08 15:07:14 -0700

    Added add method to binary search tree

--- data-structures/binary-search-tree.js
+++ data-structures/binary-search-tree.js
@@ -28,4 +28,48 @@ function BinarySearchTree() {
     }
     return max;
   };
+
+  this.add = function(num) {
+    function add(num, tree) {
+      // base case
+      if (num === tree.value) {
+        return null;
+      } else if (num < tree.value) {
+        if (tree.left === null) {
+          tree.left = new Node(num);
+          return;
+        } else {
+          return add(num, tree.left);
+        }
+      } else {
+        if (tree.right === null) {
+          tree.right = new Node(num);
+          return;
+        } else {
+          return add(num, tree.right);
+        }
+      }
+    }
+    
+    if (this.root === null) {
+      this.root = new Node(num);
+      return;
+    } else {
+      return add(num, this.root);
+    }
+    
+  } 
 }
+
+//////////////////////////////
+
+const test = new BinarySearchTree();
+test.add(4);
+test.add(1);
+test.add(7);
+test.add(87);
+test.add(34);
+test.add(45);
+test.add(73);
+test.add(8);
+console.log(displayTree(test))
