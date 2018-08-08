92f1771e0c02570ff0354997fd0807bfe8f52f04
Kyle <k.yasumiishi@gmail.com>
2018-08-08 13:31:25 -0700

    Implemented add and remove methods to doubly-linked-list

--- data-structures/doubly-linked-list.js
+++ data-structures/doubly-linked-list.js
@@ -21,7 +21,6 @@ const DoublyLinkedList = function() {
 
   // Add given element to list
   this.add = function(element) {
-
     const node = new Node(element, null);
     if (this.length > 0) {
       this.tail.next = node;
@@ -31,40 +30,55 @@ const DoublyLinkedList = function() {
       this.head = node;
       this.tail = node;
     }
-
     this.length++;
-
     return node;
-
   }
 
   // Remove all occurrences of given element in list
   this.remove = function(element) {
-
     if (this.head === null) {
       return null;
-    } else if (this.tail === null && this.head.data === element) {
+    } 
+    else if (this.head.data === element && this.length === 1) {
       this.head = null;
-    } else {
+    } 
+    else {
       let currentElement = this.head;
       let prevElement = null;
       while (currentElement.next) {
-        if (currentElement.next.data === element) {
-          prevElement.next = currentElement.next.next;
-        } else {
+        // Remove head node
+        if (currentElement.data === element && prevElement === null) {
+          currentElement = currentElement.next;
+          this.head = currentElement;
+        }
+        // Remove tail node 
+        else if (currentElement.next.data === element && currentElement.next.next === null) {
+          this.tail = currentElement;
+          currentElement.next = null;
+        }
+        // Remove node
+        else if (currentElement.data === element) {
+          prevElement.next = currentElement.next;
+          currentElement.next.prev = prevElement;
+          currentElement = currentElement.next;
+        } 
+        // Go to next node
+        else {
           prevElement = currentElement;
           currentElement = currentElement.next;
         }
       }
     }
   }
-
 };
 
+/////////////////////////////////////////////////////////
 
 const test = new DoublyLinkedList();
 
 console.log(test.add(5));
 console.log(test.add(6));
+console.log(test.remove(6))
+console.log(test.print())
 
 module.exports = DoublyLinkedList;
